367280456b20403b880cf648d74847871e776c65
= <srv.nxr@gmail.com>
2017-08-22 21:40:38 +0530

    login via cookie also can trigger cart data swapping

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,26 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use Auth;
+use App\Cart;
+use Illuminate\Support\Facades\Session;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+	    if(Auth::guard('web')->check())
+	    {
+	    	if(! Session::has('cart_token'))
+	    	{
+	    		$in_cart = Cart::where('user_id', Auth::user()->id);
+	    		if($in_cart->count() > 0)
+	    		{
+	    			Session::put('cart_token', $in_cart->first()->cart_token);
+	    		}
+	    	}
+	    }
+	}
 }
