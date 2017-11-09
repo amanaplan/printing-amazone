20254259f34f33582fa4716abe8336cd212a6052
snipelixir <srv.nxr@gmail.com>
2017-11-09 18:14:21 +0530

    artwork approval page design in progress

--- resources/views/frontend/artwork-approve.blade.php
+++ resources/views/frontend/artwork-approve.blade.php
@@ -23,6 +23,7 @@
 {{-- page specific styles --}}
 @push( 'styles' )
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/artwork-review.css') }}" />
+	<link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
 
 @endpush
 
@@ -66,8 +67,42 @@
 
 					<div class="uploaded-artwork">
 						<h4>Artwork Uploaded by You:</h4>
-						<img class="img-responsive" src="{{ asset('storage/artworks/IlzYrzwDzivlphZ4W2oqDJMPqIYxETOfnN2b1tTK.jpeg') }}" />
+						<img class="artwork-img img-responsive" onclick="showLargeImage(this);" src="{{ asset('storage/artworks/IlzYrzwDzivlphZ4W2oqDJMPqIYxETOfnN2b1tTK.jpeg') }}" />
 					</div>
+
+					<div class="clearfix"></div>
+
+					<div class="row">
+						<div class="page-header">
+							<h1 id="timeline">Timeline</h1>
+						</div>
+						<ul class="timeline">
+							<li class="timeline-inverted">
+								<div class="timeline-badge info"><i class="fa fa-commenting-o" aria-hidden="true"></i></div>
+								<div class="timeline-panel">
+									<div class="timeline-heading">
+									  	<h4 class="timeline-title">Wed, 10th Nov 17</h4>
+									</div>
+									<div class="timeline-body">
+									  	<p>I want it little more colorful and glossy, & remove the border make it runded corner</p>
+									</div>
+								</div>
+							</li>
+
+							<li class="timeline-inverted">
+								<div class="timeline-badge success"><i class="fa fa-picture-o" aria-hidden="true"></i></div>
+								<div class="timeline-panel">
+									<div class="timeline-heading">
+									  	<h4 class="timeline-title">Sat, 11th Nov 17</h4>
+									</div>
+									<div class="timeline-body">
+									  	<img src="{{ asset('storage/Small-17.jpg') }}" onclick="showLargeImage(this);" class="artwork-img thumbnail img-responsive">
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+
 				</div>
 			</div>
 
@@ -77,18 +112,30 @@
 
 					<h3 id="mockup-ready">GENERATED MOCKUP</h3>
 
-					<div class="mockup">
-						<p class="text-center">
-							<img class="img-responsive" src="{{ asset('storage/artworks/3UD5G2JndA7dfLYwTO7Zi7BCdge1fbEnoCTPjndr.png') }}" />
-						</p>
+					<div class="row">
+						<div class="mockup">
+							<p class="text-center">
+								<img class="artwork-img img-responsive" onclick="showLargeImage(this);" src="{{ asset('storage/temp.jpg') }}" />
+							</p>
+						</div>
 					</div>
 
-					<div class="adjustment-form">
-						
+					<div class="row">
+						<div class="adjustment-form">
+							<input class="btn btn-success btn-pill d-flex ml-auto mr-auto" type="submit" value="Approve Mockup">
+							<a href="">or, Make an Adjustment</a>
+						</div>
 					</div>
 				</div>
 				
 			</div>
+		</div>
+
+		<!-- The Modal -->
+		<div id="myModal" class="mymodal">
+		  <span class="close">&times;</span>
+		  <img class="mymodal-content" id="img01">
+		  <div id="caption"></div>
 		</div>
 
 		<div class="clearfix"></div>
@@ -101,5 +148,26 @@
 {{-- page specific scripts --}}
 @push( 'scripts' )
 	
+<script>
+	function showLargeImage(img)
+	{
+		// Get the modal
+		var modal = document.getElementById('myModal');
+		// Get the image and insert it inside the modal - use its "alt" text as a caption
+		var modalImg = document.getElementById("img01");
+		var captionText = document.getElementById("caption");
+		modal.style.display = "block";
+		modalImg.src = img.src;
+		captionText.innerHTML = img.alt;
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() { 
+		    modal.style.display = "none";
+		}
+	}
+</script>
+
 
 @endpush
