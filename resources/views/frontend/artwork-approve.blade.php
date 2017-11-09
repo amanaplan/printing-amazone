78da4d2a8ed2b8aac1412b75b627542a70c2b67b
snipelixir <srv.nxr@gmail.com>
2017-11-09 14:58:22 +0530

    artwork page design in progress

--- resources/views/frontend/artwork-approve.blade.php
+++ resources/views/frontend/artwork-approve.blade.php
@@ -22,7 +22,7 @@
 
 {{-- page specific styles --}}
 @push( 'styles' )
-	
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/artwork-review.css') }}" />
 
 @endpush
 
@@ -35,37 +35,58 @@
 		<div class="row">
 			<div class="bg-default col-sm-12 col-md-4 col-lg-4">
 
-				<div class="row">
-					<div class="col-sm-12">
-						<p>
-							<strong>Order ID:</strong>
-							<span class="pull-right">123456789</span>
-						</p>
+				<div class="art-card">
+					<div class="row cart-dtls">
+						<div class="col-sm-12">
+							<p>
+								<strong>Order ID:</strong>
+								<span class="pull-right text-info">123456789</span>
+							</p>
+						</div>
+					</div>
+
+					<div class="row cart-dtls">
+						<div class="col-sm-12 col-md-6">
+							<div class="pull-left">
+								<a href="#">
+									<img class="img-rounded img-responsive pull-right" src="http://printingamazon.dev/assets/images/products/Square-Stickers.png">
+									<br/>
+									<p>Square/Rectangle</p>
+								</a>
+							</div>
+						</div>
+
+						<div class="col-sm-12 col-md-6">
+							<div class="pull-right text-left order-spec">
+								<p>50 x 50 mm</p>
+								<p>Glossy Paper</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="uploaded-artwork">
+						<h4>Artwork Uploaded by You:</h4>
+						<img class="img-responsive" src="{{ asset('storage/artworks/IlzYrzwDzivlphZ4W2oqDJMPqIYxETOfnN2b1tTK.jpeg') }}" />
 					</div>
 				</div>
-
-				<div class="row">
-					<div class="col-sm-12">
-						<p>
-							<strong>Product:</strong>
-							<span class="pull-right">
-								<img class="img-rounded img-responsive pull-right" src="http://printingamazon.dev/assets/images/products/Square-Stickers.png">
-								<br>
-								<p>Square/Rectangle</p>
-							</span>
-						</p>
-					</div>
-				</div>
-
-				<h4>Artwork Uploaded by You:</h4>
-				<img class="img-responsive" src="{{ asset('storage/artworks/IlzYrzwDzivlphZ4W2oqDJMPqIYxETOfnN2b1tTK.jpeg') }}" />
 			</div>
 
 			<div class="col-sm-12 col-md-8 col-lg-8">
 
-				<p class="text-center">
-					<img class="img-responsive" src="{{ asset('storage/artworks/3UD5G2JndA7dfLYwTO7Zi7BCdge1fbEnoCTPjndr.png') }}" />
-				</p>
+				<div class="art-card">
+
+					<h3 id="mockup-ready">GENERATED MOCKUP</h3>
+
+					<div class="mockup">
+						<p class="text-center">
+							<img class="img-responsive" src="{{ asset('storage/artworks/3UD5G2JndA7dfLYwTO7Zi7BCdge1fbEnoCTPjndr.png') }}" />
+						</p>
+					</div>
+
+					<div class="adjustment-form">
+						
+					</div>
+				</div>
 				
 			</div>
 		</div>
